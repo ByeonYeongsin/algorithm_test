5cca0e15819a98b77cb13b3ef1b4f1f1833dd406
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-04-13 20:28:27 +0900

    Add problems

--- 8_two_pointer/01_21921.py
+++ 8_two_pointer/01_21921.py
@@ -2,24 +2,49 @@
 
 import sys
 
-n, x = map(int, sys.stdin.readline().strip().split())
-vis_list = list(map(int, sys.stdin.readline().strip().split()))
+# n, x = map(int, sys.stdin.readline().strip().split())
+# vis_list = list(map(int, sys.stdin.readline().strip().split()))
 
-max_count = 0
-now = 0
-for i in range(0, x):
-    now += vis_list[i]
+# max_count = 0
+# now = 0
+# for i in range(0, x):
+#     now += vis_list[i]
 
-if max(vis_list) == 0:
-    print('SAD')
+# if max(vis_list) == 0:
+#     print('SAD')
+# else:
+#     for i in range(1, len(vis_list)-x+1):
+#         if vis_list[i-1] == vis_list[i+x-1]:
+#             max_count += 1
+#         elif vis_list[i-1] < vis_list[i+x-1]:
+#             now -= vis_list[i-1]
+#             now += vis_list[i+x-1]
+#             max_count = 1
+
+#     print(now)
+#     print(max_count)
+
+input = sys.stdin.readline
+N, X = map(int, input().split())
+data = list(map(int, input().split()))
+
+if max(data) == 0:
+    print("SAD")
 else:
-    for i in range(1, len(vis_list)-x+1):
-        if vis_list[i-1] == vis_list[i+x-1]:
-            max_count += 1
-        elif vis_list[i-1] < vis_list[i+x-1]:
-            now -= vis_list[i-1]
-            now += vis_list[i+x-1]
-            max_count = 1
+    value = sum(data[:X])
 
-    print(now)
-    print(max_count)
+    max_value = value
+    max_cnt = 1
+
+    for i in range(X, N):
+        value += data[i]
+        value -= data[i-X]
+
+        if value > max_value:
+            max_value = value
+            max_cnt = 1
+        
+        elif value == max_value:
+            max_cnt += 1
+    print(max_value)
+    print(max_cnt)
