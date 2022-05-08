936f0b06011fdbe93f8095b16d93ade33ae72c5e
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-05-08 15:55:56 +0900

    Add problems

--- 20_disjoint_set/00_1717.py
+++ 20_disjoint_set/00_1717.py
@@ -1,28 +1,57 @@
 # 집합의 표현
-import sys
+# import sys
+
+# n, m = map(int, sys.stdin.readline().strip().split())
+# n_list = [0] * (n+1)
+# now_index = 1
+
+# for _ in range(m):
+#     ch, a, b = map(int, sys.stdin.readline().strip().split())
+#     if ch == 0:
+#         if a == b:
+#             continue
+#         if n_list[a] != 0:
+#             now_a = n_list[a]
+#             if n_list[b] == 0:
+#                 n_list[b] = now_a
+#             else:
+#                 now_b = n_list[b]
+#                 for i in range(n+1):
+#                     if n_list[i] == now_b:
+#                         n_list[i] = now_a
+#                 n_list[b] = now_a
+#         elif n_list[b] != 0: # n_list[a]는 0
+#             n_list[a] = n_list[b]
+#         else:
+#             n_list[a] = n_list[b] = now_index
+#             now_index += 1
+#     else:
+#         if n_list[a] != 0 and n_list[b] != 0 and n_list[a] == n_list[b]:
+#             print('YES')
+#         else:
+#             print('NO')
+
+import sys 
+sys.setrecursionlimit(1000000)
 
 n, m = map(int, sys.stdin.readline().strip().split())
-n_list = [0] * (n+1)
+parent = [i for i in range(n+1)]
 
-for i in range(m):
+def find_parent(x):
+    if parent[x] == x:
+        return x
+    parent[x] = find_parent(parent[x]) # 이해, 중요
+    return parent[x]
+
+for _ in range(m):
     ch, a, b = map(int, sys.stdin.readline().strip().split())
     if ch == 0:
-        if a == b:
+        if find_parent(a) == find_parent(b):
             continue
-        if n_list[a] != 0:
-            for j in range(len(n_list)):
-                if n_list[b] == n_list[j] and n_list[j] != 0:
-                    n_list[j] = n_list[a]
-            n_list[b] = n_list[a]
-        elif n_list[b] != 0:
-            for j in range(len(n_list)):
-                if n_list[a] == n_list[j] and n_list[j] != 0:
-                    n_list[j] = n_list[b]
-            n_list[a] = n_list[b]
-        else:
-            n_list[a] = n_list[b] = max(n_list) + 1
+        parent[find_parent(b)] = parent[find_parent(a)] # *
+        # parent[b] = parnet[a] XX
     else:
-        if n_list[a] != 0 and n_list[b] != 0 and n_list[a] == n_list[b]:
-            print('YES')
+        if find_parent(a) == find_parent(b):
+            print('yes')
         else:
-            print('NO')
+            print('no')
--- 20_disjoint_set/01_1976.py
+++ 20_disjoint_set/01_1976.py
@@ -1,3 +1,37 @@
 # 여행 가자
+from pickle import FALSE, TRUE
 import sys
+sys.setrecursionlimit(1000000)
 
+from torch import true_divide
+
+n = int(sys.stdin.readline().strip())
+m = int(sys.stdin.readline().strip())
+cities = []
+for _ in range(n):
+    cities.append(list(map(int, sys.stdin.readline().strip().split())))
+city_parent = [i for i in range(n+1)]
+
+def get_parent(n):
+    if city_parent[n] != n:
+        city_parent[n] = get_parent(city_parent[n])
+    return city_parent[n]
+
+for i in range(n):
+    for j in range(i+1, n):
+        if int(cities[i][j]) == 1:
+            city_parent[get_parent(j)] = get_parent(i)
+
+plan = set(get_parent(map(int, sys.stdin.readline().strip().split())))
+flag = 1
+# for i in range(m-1):
+#     if city_parent[get_parent(plan[i]-1)] != city_parent[get_parent(plan[i+1]-1)]:
+#         flag = 0
+#         print('NO')
+#         break
+# if flag == 1:
+#     print('YES')
+if len(plan) == 1:
+    print('YES')
+else:
+    print('NO')
