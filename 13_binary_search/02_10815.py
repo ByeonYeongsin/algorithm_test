470702d7dd4b959e0ef2728bef14c6576026c786
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-07-14 08:09:03 +0900

    Add problems

--- 13_binary_search/02_10815.py
+++ 13_binary_search/02_10815.py
@@ -1,1 +1,27 @@
 # 숫자 카드
+
+import sys
+
+def binarysearch(n_nums, key, start, end):
+    if start > end:
+        return 0
+    else:
+        mid = (start+end)//2
+        if n_nums[mid] == key:
+            return 1
+        elif n_nums[mid] < key:
+            return binarysearch(n_nums, key, mid+1, end)
+        elif n_nums[mid] > key:
+            return binarysearch(n_nums, key, start, mid-1)
+
+
+N = int(sys.stdin.readline().strip())
+n_nums = list(map(int, sys.stdin.readline().strip().split()))
+
+M = int(sys.stdin.readline().strip())
+m_nums = list(map(int, sys.stdin.readline().strip().split()))
+
+n_nums.sort()
+
+for now_m in m_nums:
+    print(binarysearch(n_nums, now_m, 0, len(n_nums)-1), end=' ')
--- 13_binary_search/03_2805.py
+++ 13_binary_search/03_2805.py
@@ -1,1 +1,21 @@
 # 나무 자르기
+
+import sys
+
+def binarysearch(trees, M, start, end):
+    mid = (start + end) // 2
+    tmp = 0
+    for tree in trees:
+        if tree > mid:
+            tmp += (tree-mid)
+    if tmp == M:
+        return mid
+    if tmp < M:
+        return binarysearch(trees, M, start, mid-1)
+    if tmp > M:
+        return binarysearch(trees, M, mid+1, end)
+
+N, M = map(int, sys.stdin.readline().strip().split())
+trees = list(map(int, sys.stdin.readline().strip().split()))
+
+print(binarysearch(trees, M, 0, max(trees)))
