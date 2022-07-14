435434182316c7cea905a0f437b7219ba591486d
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-07-14 21:29:21 +0900

    Add problems

--- 15_divide_and_conquer/02_18222.py
+++ 15_divide_and_conquer/02_18222.py
@@ -1,1 +1,28 @@
 # 투에-모스 문자열
+import sys
+import math
+
+def do_toemos(n, k):
+    if n == 1:
+        print(k) 
+        return
+
+    num = math.log(n, 2)
+    if num-int(num) == 0: # 완벽히 나누어 떨어지는 경우
+        if num % 2 == 0:
+            print(k)
+            return
+        else:
+            print((k+1)%2)
+            return
+    else:
+        num = int(num)
+
+    if num % 2 == 0:
+        do_toemos(n-2**num, (k+1)%2)
+    else:
+        k = (k+1) % 2
+        do_toemos(n-2**num, k)
+
+k = int(sys.stdin.readline().strip())
+do_toemos(k, 0)
--- 15_divide_and_conquer/03_2447.py
+++ 15_divide_and_conquer/03_2447.py
@@ -1,4 +1,23 @@
-# 별 찍기
+# 별찍기
 import sys
+import math
 
-k = int(sys.stdin.readline().strip())
+def get_stars(n):
+    starts = []
+    for i in range(3 * len(n)):
+        # n(즉 len(stars))이 3으로 나누어 떨어지지 않는다면(1이 남는다면) 가운데 공백을 줌(n의 길이 만큼)
+        if i // len(n) ==  1:
+            starts.append(n[i % len(n)] + " " * len(n) + n[i % len(n)])
+        # n이 3으로 나누어 떨어진다면, 공백 없이 가득 채움
+        else:
+            starts.append(n[i % len(n)] * 3)
+    return starts
+
+n = int(sys.stdin.readline().strip())
+stars_basic = ["***", "* *", "***"]
+k = int(math.log(n, 3)) - 1
+
+for i in range(k):
+    stars = get_stars(stars_basic)
+for i in stars:
+    print(i)
