01238989ca42622a05a87116bd8d8cc9f0f71d9c
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-07-21 09:04:25 +0900

    Add problems

--- 18_shortest_path/02_11265.py
+++ 18_shortest_path/02_11265.py
@@ -1,3 +1,20 @@
 # 끝나지 않는 파티
 import sys
 
+N, M = map(int, sys.stdin.readline().strip().split())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, sys.stdin.readline().strip().split())))
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if graph[j][i] + graph[i][k] < graph[j][k]:
+                graph[j][k] = graph[j][i] + graph[i][k]
+
+for _ in range(M):
+    A, B, C = map(int, sys.stdin.readline().strip().split())
+    if graph[A-1][B-1] <= C:
+        print('Enjoy other party')
+    else:
+        print('Stay here')
