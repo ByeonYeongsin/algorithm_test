23d40c482cc34051d7f0f9f4efc6f6b21a09c839
ByeonYeongsin <55366334+ByeonYeongsin@users.noreply.github.com>
2022-08-18 21:49:20 +0900

    Add problems

--- 21_minimum_spanning_tree/03_16398.py
+++ 21_minimum_spanning_tree/03_16398.py
@@ -1,12 +1,12 @@
 # 행성 연결
 import sys
 
-def find_parent(parent, x):  # 부모 노드 찾기
+def find_parent(parent, x):
     if parent[x] != x:
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union(parent, a, b):  # 집합 합치기
+def union(parent, a, b): 
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
@@ -14,10 +14,10 @@ def union(parent, a, b):  # 집합 합치기
     else:
         parent[a] = b
 
-n = int(sys.stdin.readline().strip().split())  # 행성의 수
-arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]  # 각 행성간의 플로우 관리 비용
-parent = [i for i in range(n)]  # 부모 노드 정보
-edges = []  # 행성 간선 정보 # 삼각형 대칭
+n = int(sys.stdin.readline().strip()) 
+arr = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]  
+parent = [i for i in range(n)] 
+edges = []  
 
 for i in range(1, n):
     for j in range(i):
@@ -25,9 +25,9 @@ for i in range(1, n):
 edges.sort()
 
 answer = 0
-for cost, a, b in edges:  # 크루스칼 알고리즘 수행
-    if find_parent(parent, a) != find_parent(parent, b):  # 서로 같은 집합이 아닌 경우
-        union(parent, a, b)  # 연결
+for cost, a, b in edges: 
+    if find_parent(parent, a) != find_parent(parent, b): 
+        union(parent, a, b) 
         answer += cost
 
-print(answer)  # 최소 플로우의 관리 비용
+print(answer) 
